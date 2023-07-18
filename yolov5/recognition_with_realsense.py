0f89ae0c4e4be6123e37cba025aad160ba4d73e8
Yigit Sirin <122676073+neraiv@users.noreply.github.com>
2023-07-18 10:02:13 +0300

    Add files via upload

---
+++ yolov5/recognition_with_realsense.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import os, sys
+import cv2
+import pyrealsense2 as rs
+import numpy as np
+import time
+from pathlib import Path
+import torch
+import torch.backends.cudnn as cudnn
+import math
+
+FILE = Path(__file__).absolute()
+sys.path.append(FILE.parents[0].as_posix())
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.augmentations import Albumentations, augment_hsv, copy_paste, letterbox, mixup, random_perspective
+from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
+    apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import Annotator, colors
+from utils.torch_utils import  select_device, time_sync
+
+@torch.no_grad()
+def run():
+
+     # run once
+
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+    pipeline = rs.pipeline()
+    profile = pipeline.start(config)
+
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+    
+    while(True):
+        t0 = time.time()
+
+        frames = pipeline.wait_for_frames()
+
+        aligned_frames = align.process(frames)
+        color_frame = aligned_frames.get_color_frame()
+        depth_frame = aligned_frames.get_depth_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        img = np.asanyarray(color_frame.get_data())
+
+
+        
+        cv2.imshow("stacked", img)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break  
+
+if __name__ == '__main__':
+    run()
